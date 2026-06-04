3fada9e7d30536b6ab358f303816478f04b26b30
Nadia Gayle <nadiagayle@yail.com>
2026-06-04 09:43:57 -0400

    update cidr for port 22

--- .history/ec2/ec2_20260604094239.tf
+++ .history/ec2/ec2_20260604094239.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.1/16"]
+    cidr_blocks = ["10.0.0./16"]
   }
 
   ingress {
